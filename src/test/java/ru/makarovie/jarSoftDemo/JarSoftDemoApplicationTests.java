05ca5cdad9a7e3e5426dec700e2a6e4cc0d5857c
IvamMak <makarovie@yandex.ru>
2020-08-25 14:48:23 +0700

    add project structure folders

---
+++ src/test/java/ru/makarovie/jarSoftDemo/JarSoftDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.makarovie.jarSoftDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarSoftDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
